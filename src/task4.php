633b68c82bab5eab2561052f58a066a46a9a3cce
Kehet <Kehet@users.noreply.github.com>
2025-02-14 12:27:35 +0200

    Tehtävä 4: Lukujen summa

---
+++ src/task4.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * Tässä tehtävässä täytyy laskea yhteen annetut luvut. Esimerkiksi jos luvut ovat 3, 8 ja 4, niiden summa on 15. Luvut ovat positiivisia kokonaislukuja, ja niiden summa on alle miljoona.
+ */
+
+assertTrue(isset($_REQUEST['luvut']));
+
+$luvut = explode('|', $_REQUEST['luvut']);
+
+echo array_reduce($luvut, static function ($carry, $item) {
+    return $carry + $item;
+}, 0);
